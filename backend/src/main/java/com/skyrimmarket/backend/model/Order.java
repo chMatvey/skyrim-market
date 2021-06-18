41933eb91d5806272a6534b62c2ee0eb304a37e5
vaqober <vaqober@mail.ru>
2021-06-18 13:48:00 +0300

    user crud api, order get methods

--- backend/src/main/java/com/skyrimmarket/backend/model/Order.java
+++ backend/src/main/java/com/skyrimmarket/backend/model/Order.java
@@ -1,5 +1,7 @@
 package com.skyrimmarket.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.skyrimmarket.backend.model.user.Client;
 import com.skyrimmarket.backend.model.user.Employee;
 import lombok.*;
@@ -42,10 +44,12 @@ public class Order {
 
     @ManyToOne
     @JoinColumn(name = "client_id", nullable = false)
+    @JsonBackReference
     private Client client;
 
     @ManyToOne
     @JoinColumn(name = "employee_id")
+    @JsonBackReference
     private Employee contractor;
 
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
