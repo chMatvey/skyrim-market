330e0f96fddfa5056634da43dc5a9af25eab8068
vaqober <vaqober@mail.ru>
2022-01-19 12:06:15 +0300

    add created employee test

--- backend/src/test/java/com/skyrimmarket/backend/service/UserServiceImplTest.java
+++ backend/src/test/java/com/skyrimmarket/backend/service/UserServiceImplTest.java
@@ -1,9 +1,12 @@
 package com.skyrimmarket.backend.service;
 
 import com.skyrimmarket.backend.model.user.Client;
+import com.skyrimmarket.backend.model.user.Employee;
 import com.skyrimmarket.backend.model.user.SkyrimUser;
+import com.skyrimmarket.backend.model.user.Student;
 import com.skyrimmarket.backend.repository.UserRepository;
 import com.skyrimmarket.backend.web.error.UsernameAlreadyExist;
+import com.skyrimmarket.backend.web.form.EmployeeForm;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,7 +46,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void notCreated(){
+    void notCreated() {
         SkyrimUser user = new Client();
         when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
         when(passwordEncoder.encode(user.getPassword())).thenReturn("1");
@@ -51,6 +54,30 @@ class UserServiceImplTest {
 
         when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.of(user));
         assertThrows(UsernameAlreadyExist.class, () -> userService.create(user));
+    }
+
+    @Test
+    void createdStudent() {
+        SkyrimUser user = new Student();
+        SkyrimUser mentor = new Employee();
+        EmployeeForm employeeForm = new EmployeeForm(user.getUsername(), user.getPassword(), true, mentor.getId());
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
+        when(userRepository.findById(employeeForm.getMentorId())).thenReturn(Optional.of(mentor));
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("1");
+        userService.createEmployee(employeeForm);
+
+        verify(userRepository).saveAndFlush(user);
+    }
+
+    @Test
+    void createdEmployee() {
+        SkyrimUser user = new Employee();
+        EmployeeForm employeeForm = new EmployeeForm(user.getUsername(), user.getPassword(), false, null);
+        when(userRepository.findByUsername(user.getUsername())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("1");
+        userService.createEmployee(employeeForm);
+
+        verify(userRepository).saveAndFlush(user);
     }
 
     @Test
@@ -64,7 +91,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void notLoadedUserByUsername(){
+    void notLoadedUserByUsername() {
         String username = "tatata";
         when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
         assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername(username));
